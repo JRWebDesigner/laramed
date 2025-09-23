83755f6df9cfd3fc49050dc1a77ef27716d75153
Mamani Agreda Josue Martin <jowebdesigc@gmail.com>
2025-09-23 13:06:45 -0400

    asd

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,22 @@
 export default function Footer(){
   return(
-    <header>
-      
-    </header>
+    <footer className="bg-blue-950 h-[50dvh] grid items-center md:grid-cols-3 text-white">
+      <img src="/logob.png" className="w-[300px]" />
+      <div>
+        <h2>INFORMACION</h2>
+        <h3>
+            Direccion:
+        </h3>
+        <h3>
+            Telefono:
+        </h3>
+        <h3>
+            Horario de atencion:
+        </h3>
+      </div>
+      <div>
+        <h2>REDES SOCIALES</h2>
+      </div>
+    </footer>
   );
 }
