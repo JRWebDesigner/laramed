751eee4063adda3e2e20126c18aee08800082d28
Mamani Agreda Josue Martin <jowebdesigc@gmail.com>
2025-09-27 10:37:08 -0400

    historia

--- src/app/sobre-nosotros/page.tsx
+++ src/app/sobre-nosotros/page.tsx
@@ -15,23 +15,24 @@ export default function sobreNosotros(){
         <h1 className="z-20 font-bold text-center text-4xl md:text-6xl container mx-auto text-shadow-lg">Laramed S.R.L: Importadora y Distribuidora de Equipos Médicos en Bolivia</h1>
       </section>  
       <section className="container mx-auto my-22 px-5">
-        <h2 className='text-4xl font-bold'>Nuestra Historia</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit tempus, nec integer dis sem vulputate maecenas faucibus nascetur dignissim, mus eros primis blandit in aliquam vestibulum. Erat commodo fusce ut cum facilisi suscipit dui elementum, pretium urna blandit class ad porta placerat, nostra primis eget imperdiet vivamus convallis donec. Ac nunc curabitur massa mollis justo cum per erat hac, porta etiam varius accumsan natoque non hendrerit blandit, nisl parturient neque aliquet leo primis eu maecenas.</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit tempus, nec integer dis sem vulputate maecenas faucibus nascetur dignissim, mus eros primis blandit in aliquam vestibulum. Erat commodo fusce ut cum facilisi suscipit dui elementum, pretium urna blandit class ad porta placerat, nostra primis eget imperdiet vivamus convallis donec. Ac nunc curabitur massa mollis justo cum per erat hac, porta etiam varius accumsan natoque non hendrerit blandit, nisl parturient neque aliquet leo primis eu maecenas.</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit tempus, nec integer dis sem vulputate maecenas faucibus nascetur dignissim, mus eros primis blandit in aliquam vestibulum. Erat commodo fusce ut cum facilisi suscipit dui elementum, pretium urna blandit class ad porta placerat, nostra primis eget imperdiet vivamus convallis donec. Ac nunc curabitur massa mollis justo cum per erat hac, porta etiam varius accumsan natoque non hendrerit blandit, nisl parturient neque aliquet leo primis eu maecenas.</p>
+        <h2 className='text-4xl font-bold mb-4'>¿Quiénes Somos?</h2>
+        <p className="text-lg">Laramed S.R.L es una empresa importadora y distribuidora de equipo e insumo medico, nos enfocamos en las areas de: EMERGENCIAS, QUIROFANO, TERAPIA INTENSIVA y HERMODIALISIS.</p>
       </section>
-      <section className='relative container mx-auto grid md:grid-cols-2 place-items-center my-22 px-5 text-white'>
-        <div className="relative bg-[url('/mision.jpg')] bg-norepeat bg-cover">
-          <div className="absolute bg-[#000] h-full w-full opacity-60 z-0"/>
-          <h3 className="relative z-10 text-3xl font-semibold p-6">Mision</h3>
-          <p className="relative z-10 p-6">Lorem ipsum dolor sit amet consectetur adipiscing elit tempus, nec integer dis sem vulputate maecenas faucibus nascetur dignissim, mus eros primis blandit in aliquam vestibulum. Erat commodo fusce ut cum facilisi suscipit dui elementum, pretium urna blandit class ad porta placerat, nostra primis eget imperdiet vivamus convallis donec. Ac nunc curabitur massa mollis justo cum per erat hac, porta etiam varius accumsan natoque non hendrerit blandit, nisl parturient neque aliquet leo primis eu maecenas.</p>
+      <section className='relative container mx-auto grid md:grid-cols-3 place-items-center my-22 px-5 text-white'>
+        <div className="rounded-t-2xl md:rounded-t-none md:rounded-l-2xl w-full h-full relative bg-[url('/mision.jpg')] bg-norepeat bg-cover">
+          <div className="rounded-t-2xl md:rounded-t-none md:rounded-l-2xl absolute bg-[#000] h-full w-full opacity-60 z-0"/>
+          <h3 className="relative z-10 text-3xl font-semibold p-6">Misión</h3>
+          <p className="relative z-10 p-6 text-justify text-lg">Ser un referente nacional, en suministro de equipo e insumo medico de alta tecnología, garantizando la calidad y soporte técnico oportuno, trabajando siempre el cuidado de la salud de los pacientes de nuestros clientes.</p>
         </div>
-        <div className="relative bg-[url('/mision.jpg')] bg-norepeat bg-cover bg-[url('/vision.jpg')]">
+        <div className="relative w-full h-full bg-[url('/mision.jpg')] bg-norepeat bg-cover bg-[url('/vision.jpg')]">
           <div className="absolute bg-[#000] h-full w-full opacity-40 z-0"/>
-          <h3 className="relative z-10 text-3xl font-semibold p-6">Vision</h3>
-          <p className="relative z-10 p-6">Lorem ipsum dolor sit amet consectetur adipiscing elit tempus, nec integer dis sem vulputate maecenas faucibus nascetur dignissim, mus eros primis blandit in aliquam vestibulum. Erat commodo fusce ut cum facilisi suscipit dui elementum, pretium urna blandit class ad porta placerat, nostra primis eget imperdiet vivamus convallis donec. Ac nunc curabitur massa mollis justo cum per erat hac, porta etiam varius accumsan natoque non hendrerit blandit, nisl parturient neque aliquet leo primis eu maecenas.</p>
+          <h3 className="relative z-10 text-3xl font-semibold p-6">Visión</h3>
+          <p className="relative z-10 p-6 text-justify text-lg">Promover y actualizar a nuestra población al uso de nuevas tecnologías medicas en todo el territorio Boliviano</p>
+        </div>
+        <div className="rounded-b-2xl md:rounded-b-none md:rounded-r-2xl w-full h-full relative bg-[url('/mision.jpg')] bg-norepeat bg-cover bg-[url('/mision.jpg')]">
+          <div className="rounded-b-2xl md:rounded-b-none md:rounded-r-2xl absolute bg-[#000] h-full w-full opacity-40 z-0"/>
+          <h3 className="relative z-10 text-3xl font-semibold p-6">Valores</h3>
+          <p className="relative z-10 p-6 text-justify text-lg">Honestidad, Pasión, Puntualidad, Responsabilidad y Calidad</p>
         </div>
       </section>
       <section className="container mx-auto flex items-center flex-col my-22 px-5">
