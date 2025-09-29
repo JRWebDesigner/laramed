1f82bb04bc86c803a3d3fba25d4f86842892781b
Mamani Agreda Josue Martin <jowebdesigc@gmail.com>
2025-09-29 16:11:42 -0400

    sanity

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -7,5 +7,5 @@ import {carruselPrincipal} from './carruselPrincipal'
 import {publicacion} from './publicacion'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [ blockContentType, producto, categoria, carruselPrincipal, publicacion ]],
+  types: [ blockContentType, producto, categoria, carruselPrincipal, publicacion ],
 }
