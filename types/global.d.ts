3ce5e2a342ac6b1eb0287b7b8b80620d5b803c65
Mamani Agreda Josue Martin <jowebdesigc@gmail.com>
2025-09-30 10:32:54 -0400

    final

---
+++ types/global.d.ts
@@ -0,0 +1,33 @@
+// types/global.d.ts
+declare global {
+  type Producto = {
+    _id: string;
+    nombre: string;
+    slug: string;
+    imagenPrincipal?: string;
+    imagenes?: string[];
+    destacado?: boolean;
+    descripcion?: any;
+    categoria?: {
+      nombre: string;
+      slug: string;
+    };
+  };
+
+  type Categoria = {
+    _id: string;
+    nombre: string;
+    slug: string;
+    imagen?: string;
+  };
+
+  type Publicacion = {
+    _id: string;
+    titulo: string;
+    imagen?: string;
+    descripcion?: any;
+    fecha?: string;
+  };
+}
+
+export {};
